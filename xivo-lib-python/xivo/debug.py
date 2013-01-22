db6ae6954c4eea632f8262f9af33d5f69cd9b514
Etienne Lessard <elessard@avencall.com>
2013-01-22 10:59:40 -0500

    update debug module to be more useful
    Add a trace_duration decorator. Is enabled only if the environment variable
    XIVO_DEBUG is set to a non-null string.

--- xivo-lib-python/xivo/debug.py
+++ xivo-lib-python/xivo/debug.py
@@ -3,18 +3,32 @@
 import functools
 import logging
 import time
+import os
 
 logger = logging.getLogger(__name__)
 
+DEBUG_MODE = os.environ.get('XIVO_DEBUG')
 
-def time_decorator(fun):
+
+def _debug(decorator):
+    if DEBUG_MODE:
+        return decorator
+    else:
+        return _no_op_decorator
+
+
+def _no_op_decorator(fun):
+    return fun
+
+
+@_debug
+def trace_duration(fun):
     fun_name = fun.__name__
     @functools.wraps(fun)
     def aux_fun(*args, **kwargs):
-        logger.debug('Entering %s', fun_name)
         start_time = time.time()
         result = fun(*args, **kwargs)
         duration = time.time() - start_time
-        logger.debug('Exiting %s (took %.6fs)', fun_name, duration)
+        logger.info('Execution of %r took %.3fs', fun_name, duration)
         return result
     return aux_fun
