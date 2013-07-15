22174456e1c902f74483b38defb9a1d5361067d1
Pascal Cadotte Michaud <pcadottemichaud@avencall.com>
2013-07-15 10:26:09 -0400

    Ticket #4196 add channel name parsing helper

---
+++ xivo-lib-python/xivo/asterisk/protocol_interface.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2013 Avencall
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+import collections
+import re
+
+channel_regexp = re.compile(r'(sip|sccp)/(\w+).*', re.I)
+
+
+ProtocolInterface = collections.namedtuple('ProtocolInterface', ['protocol', 'interface'])
+
+
+class InvalidChannel(ValueError):
+    def __init__(self, invalid_channel):
+        ValueError.__init__(self, 'the channel %s is invalid' % invalid_channel)
+
+
+def protocol_interface_from_channel(channel):
+    matches = channel_regexp.match(channel)
+    if matches is None:
+        raise InvalidChannel(channel)
+    protocol = matches.group(1)
+    interface = matches.group(2)
+
+    return ProtocolInterface(protocol, interface)
---
+++ xivo-lib-python/xivo/asterisk/tests/test_protocol_interface.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2013 Avencall
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+import unittest
+
+from xivo.asterisk.protocol_interface import ProtocolInterface
+from xivo.asterisk.protocol_interface import InvalidChannel
+from xivo.asterisk.protocol_interface import protocol_interface_from_channel
+
+
+class TestProtocolInterface(unittest.TestCase):
+
+    def test_protocol_interface_from_channel_sip(self):
+        channel = 'SIP/askdjhf-3216549'
+        expected_result = ProtocolInterface('SIP', 'askdjhf')
+
+        result = protocol_interface_from_channel(channel)
+
+        self.assertEquals(expected_result, result)
+
+    def test_protocol_interface_from_channel_sccp(self):
+        channel = 'SCCP/13486-00000658'
+        expected_result = ProtocolInterface('SCCP', '13486')
+
+        result = protocol_interface_from_channel(channel)
+
+        self.assertEquals(expected_result, result)
+
+    def test_protocol_interface_from_channel_invalid(self):
+        invalid_channel = 'slkdfjaslkdjfaslkdjflskdjf'
+
+        self.assertRaises(InvalidChannel, protocol_interface_from_channel, invalid_channel)
