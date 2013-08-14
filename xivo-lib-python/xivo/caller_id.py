059279b112b754eefe3c83efbad711619f0dceaa
Cedric Abunar <cabunar@avencall.com>
2013-08-14 13:51:08 -0400

    add extract_display_name from callerid

--- xivo-lib-python/xivo/caller_id.py
+++ xivo-lib-python/xivo/caller_id.py
@@ -15,20 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA..
-
 import re
 
 COMPLETE_CALLER_ID_PATTERN = re.compile('\"(.*)\" \<(\d+)\>')
@@ -50,5 +36,13 @@ def extract_number(caller_id):
     result = COMPLETE_CALLER_ID_PATTERN.search(caller_id)
     if result:
         return result.groups()[1]
+    else:
+        raise ValueError('Not a valid Caller ID: %s', caller_id)
+
+
+def extract_displayname(caller_id):
+    result = COMPLETE_CALLER_ID_PATTERN.search(caller_id)
+    if result:
+        return result.groups()[0]
     else:
         raise ValueError('Not a valid Caller ID: %s', caller_id)
---
+++ xivo-lib-python/xivo/tests/test_caller_id.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2013 Avencall
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+import unittest
+from xivo.caller_id import build_caller_id, _complete_caller_id, extract_number, \
+    extract_displayname
+
+
+class TestCallerID(unittest.TestCase):
+
+    def test_caller_id_no_num(self):
+        begin = '"User One"'
+        name = 'User One'
+        number = '123'
+
+        cid_all, cid_name, cid_number = build_caller_id(begin, name, number)
+
+        self.assertEqual(cid_all, '"%s" <%s>' % (name, number))
+        self.assertEqual(cid_name, name)
+        self.assertEqual(cid_number, number)
+
+    def test_caller_id_number(self):
+        begin = '"User One" <123>'
+        name = 'User One'
+        number = '123'
+
+        cid_all, cid_name, cid_number = build_caller_id(begin, name, number)
+
+        self.assertEqual(cid_all, '"%s" <%s>' % (name, number))
+        self.assertEqual(cid_name, name)
+        self.assertEqual(cid_number, number)
+
+    def test_complete_caller_id(self):
+        cid = '"User One" <1234>'
+
+        self.assertTrue(_complete_caller_id(cid))
+
+    def test_extract_number(self):
+        caller_id = '"User 1" <1001>'
+
+        ret = extract_number(caller_id)
+
+        self.assertEqual(ret, '1001')
+
+    def test_extract_number_not_a_caller_id(self):
+        self.assertRaises(ValueError, extract_number, '1001')
+
+    def test_extract_displayname(self):
+        caller_id = '"User 1" <1001>'
+
+        ret = extract_displayname(caller_id)
+
+        self.assertEqual(ret, 'User 1')
+
+    def test_extract_displayname_with_invalid_caller_id(self):
+        self.assertRaises(ValueError, extract_displayname, '1001')
