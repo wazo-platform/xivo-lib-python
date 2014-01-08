eab705ed35462ec408f1136d6d76a0865c32459e
Pascal Cadotte Michaud <pascal.cadotte@gmail.com>
2014-01-08 10:22:18 -0500

    update copyright years

--- xivo-lib-python/xivo/StreamedLines.py
+++ xivo-lib-python/xivo/StreamedLines.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2008-2013 Avencall
+# Copyright (C) 2008-2014 Avencall
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
