e50495507c0ba5ef1bec791446b575647d32a753
Pascal Cadotte Michaud <pcadottemichaud@avencall.com>
2015-11-13 11:26:34 -0500

    remove dead code
    remove yaml_json.unstringify_keys

--- xivo/yaml_json.py
+++ xivo/yaml_json.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2008-2014 Avencall
+# Copyright (C) 2008-2015 Avencall
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,14 +14,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
-
-"""YAML <-> JSON interoperability
-
-Copyright (C) 2008-2010  Avencall
-
-"""
-
-__version__ = "$Revision$ $Date$"
 
 
 def stringify_keys(obj):
@@ -38,35 +30,6 @@ def stringify_keys(obj):
         return map(stringify_keys, obj)
     elif isinstance(obj, dict):
         return dict(((str(k), stringify_keys(v)) for k, v in obj.iteritems()))
-    else:
-        return obj
-
-
-def _try_int(s):
-    "return int(s) if possible else s"
-    try:
-        return int(s)
-    except (TypeError, ValueError):
-        return s
-
-
-def unstringify_keys(obj):
-    """
-    Returns a *deep* copy of @obj except that keys of dictionaries are
-    converted to integers when possible.
-
-    See also stringify_keys()
-
-    WARNING: There is no warranty that
-        obj == unstringify_keys(stringify_keys(obj))
-
-    >>> unstringify_keys([{'vs_0001': {'0': 'static_0001'}}, 12])
-    [{'vs_0001': {0: 'static_0001'}}, 12]
-    """
-    if isinstance(obj, list):
-        return map(unstringify_keys, obj)
-    elif isinstance(obj, dict):
-        return dict(((_try_int(k), unstringify_keys(v)) for k, v in obj.iteritems()))
     else:
         return obj
 
