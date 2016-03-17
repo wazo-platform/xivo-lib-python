4c1a24f55a23a91d256749fdbc1df56cd3472bae
Sebastien Duthil <sduthil@proformatique.com>
2016-03-17 15:10:54 -0400

    pubsub: simplify unit tests

--- xivo/tests/test_pubsub.py
+++ xivo/tests/test_pubsub.py
@@ -18,6 +18,7 @@
 import unittest
 
 from hamcrest import assert_that
+from hamcrest import not_
 from mock import Mock
 from mock import patch
 from xivo.pubsub import Pubsub
@@ -34,26 +35,22 @@ class TestPubsub(unittest.TestCase):
 
     def test_subscribe_and_publish(self):
         callback = Mock()
-        message = Mock()
-        topic = 'abcd'
+        self.pubsub.subscribe(SOME_TOPIC, callback)
 
-        self.pubsub.subscribe(topic, callback)
+        self.pubsub.publish(SOME_TOPIC, SOME_MESSAGE)
 
-        self.pubsub.publish(topic, message)
-        callback.assert_called_once_with(message)
+        callback.assert_called_once_with(SOME_MESSAGE)
 
     def test_multiple_subscribe_on_same_topic_and_one_publish(self):
         callback_1 = Mock()
         callback_2 = Mock()
-        message = Mock()
-        topic = 'abcd'
+        self.pubsub.subscribe(SOME_TOPIC, callback_1)
+        self.pubsub.subscribe(SOME_TOPIC, callback_2)
 
-        self.pubsub.subscribe(topic, callback_1)
-        self.pubsub.subscribe(topic, callback_2)
+        self.pubsub.publish(SOME_TOPIC, SOME_MESSAGE)
 
-        self.pubsub.publish(topic, message)
-        callback_1.assert_called_once_with(message)
-        callback_2.assert_called_once_with(message)
+        callback_1.assert_called_once_with(SOME_MESSAGE)
+        callback_2.assert_called_once_with(SOME_MESSAGE)
 
     def test_multiple_subscribe_on_different_topics_and_two_publish(self):
         callback = Mock()
@@ -61,58 +58,52 @@ class TestPubsub(unittest.TestCase):
         message_2 = Mock()
         topic_1 = 'abcd'
         topic_2 = 'efgh'
-
         self.pubsub.subscribe(topic_1, callback)
         self.pubsub.subscribe(topic_2, callback)
 
         self.pubsub.publish(topic_1, message_1)
-        callback.assert_any_call(message_1)
-        self.assertEquals(callback.call_count, 1)
-
         self.pubsub.publish(topic_2, message_2)
+
+        callback.assert_any_call(message_1)
         callback.assert_any_call(message_2)
         self.assertEquals(callback.call_count, 2)
 
     def test_unsubscribe_when_never_subscribed(self):
         callback = Mock()
-        topic = 'abcd'
 
-        self.pubsub.unsubscribe(topic, callback)
-
-        # Does not raise Exception
+        try:
+            self.pubsub.unsubscribe(SOME_TOPIC, callback)
+        except Exception:
+            self.fail('unsubscribe should not raise exceptions')
+            raise
 
     def test_unsubscribed_when_subscribed(self):
         callback = Mock()
-        message = Mock()
-        topic = 'abcd'
-        self.pubsub.subscribe(topic, callback)
+        self.pubsub.subscribe(SOME_TOPIC, callback)
 
-        self.pubsub.unsubscribe(topic, callback)
+        self.pubsub.unsubscribe(SOME_TOPIC, callback)
+        self.pubsub.publish(SOME_TOPIC, SOME_MESSAGE)
 
-        self.pubsub.publish(topic, message)
         self.assertEquals(callback.call_count, 0)
 
     def publish_when_nobody_subscribed(self):
-        message = Mock()
-        topic = 'abcd'
-
-        self.pubsub.publish(topic, message)
-
-        # Does not raise Exception
+        try:
+            self.pubsub.publish(SOME_TOPIC, SOME_MESSAGE)
+        except Exception:
+            self.fail('publish should not raise exceptions')
+            raise
 
     def test_unsubscribe_when_multiple_subscribers_on_same_topic(self):
         callback_1 = Mock()
         callback_2 = Mock()
-        message = Mock()
-        topic = 'abcd'
-        self.pubsub.subscribe(topic, callback_1)
-        self.pubsub.subscribe(topic, callback_2)
+        self.pubsub.subscribe(SOME_TOPIC, callback_1)
+        self.pubsub.subscribe(SOME_TOPIC, callback_2)
 
-        self.pubsub.unsubscribe(topic, callback_1)
+        self.pubsub.unsubscribe(SOME_TOPIC, callback_1)
+        self.pubsub.publish(SOME_TOPIC, SOME_MESSAGE)
 
-        self.pubsub.publish(topic, message)
-        self.assertEquals(callback_1.call_count, 0)
-        callback_2.assert_called_once_with(message)
+        assert_that(not_(callback_1.called))
+        callback_2.assert_called_once_with(SOME_MESSAGE)
 
 
 class TestExceptionLoggingPubsub(unittest.TestCase):
