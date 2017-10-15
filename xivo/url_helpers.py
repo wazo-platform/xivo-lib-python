a2bce9c1d6a2dbe276ddbd545b075a02251ebe31
Francois Blackburn <fblackburn@wazo.community>
2017-10-15 10:33:24 -0400

    fix url_helpers python3 compatibility

--- xivo/url_helpers.py
+++ xivo/url_helpers.py
@@ -3,8 +3,9 @@
 # SPDX-License-Identifier: GPL-3.0+
 
 from requests.utils import quote
+from six import text_type
 
 
 def base_join(base, *fragments):
-    path = '/'.join(quote(unicode(fragment)).strip('/') for fragment in fragments)
+    path = '/'.join(quote(text_type(fragment)).strip('/') for fragment in fragments)
     return "{base}/{path}".format(base=base.strip('/'), path=path)
