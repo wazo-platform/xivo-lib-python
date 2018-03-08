c398e165f7f2c0eb7de2aceeacd7566dcccbec3e
Sebastien Duthil <sduthil@wazo.community>
2018-03-08 14:47:30 -0500

    add tenant_helpers

---
+++ xivo/tenant_helpers.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+import requests
+import flask
+
+from flask import request
+from xivo.auth_verifier import extract_token_id_from_header
+from xivo.auth_verifier import AuthServerUnreachable
+
+
+class InvalidTenant(Exception):
+    def __init__(self, tenant_uuid=None):
+        if tenant_uuid:
+            super().__init__('Invalid tenant "{uuid}"'.format(uuid=tenant_uuid))
+        else:
+            super().__init__('Invalid tenant')
+
+
+class InvalidToken(Exception):
+    def __init__(self, token_id=None):
+        if token_id:
+            super().__init__('Invalid token "{id}"'.format(id=token_id))
+        else:
+            super().__init__('Invalid token')
+
+
+class Tenant(object):
+
+    @classmethod
+    def autodetect(cls, headers, tokens):
+        token = tokens.from_headers()
+        try:
+            return cls.from_headers().check_against(token)
+        except InvalidTenant:
+            return cls.from_token(token)
+
+    @classmethod
+    def from_headers(cls):
+        try:
+            tenant_uuid = request.headers['X-Wazo-Tenant']
+        except KeyError:
+            raise InvalidTenant()
+        return cls(uuid=tenant_uuid)
+
+    @classmethod
+    def from_token(cls, token):
+        try:
+            tenants = token['metadata']['tenants']
+        except KeyError:
+            raise InvalidTenant()
+        if not tenants:
+            raise InvalidTenant()
+        if len(tenants) > 2:
+            raise InvalidTenant()
+        return tenants[0]
+
+    def __init__(self, uuid):
+        self.uuid = uuid
+
+    def check_against(self, token):
+        if self.uuid not in token['metadata'].get('tenants', []):
+            raise InvalidTenant(self.uuid)
+        return self
+
+
+class Tokens(object):
+
+    def __init__(self, auth):
+        self._auth = auth
+
+    def get(self, token_id):
+        try:
+            return self._auth.token.get(token_id)
+        except requests.HTTPError as e:
+            raise InvalidToken(token_id)
+        except requests.RequestException as e:
+            raise AuthServerUnreachable(self._auth.host, self._auth.port, e)
+
+    def from_headers(self):
+        token_id = extract_token_id_from_header()
+        if not token_id:
+            raise InvalidToken()
+        return self.get(token_id)
+
+
+class CachedTokens(Tokens):
+
+    def get(self, token_id):
+        token = flask.g['tokens'].get(token_id)
+        if token:
+            return token
+
+        token = flask.g['tokens'][token_id] = super().get(token_id)
+        return token
