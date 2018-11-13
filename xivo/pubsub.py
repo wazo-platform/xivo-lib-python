379a03223aae63066dd21c83e829fa8da65fabbf
Sebastien Duthil <sduthil@wazo.community>
2018-11-13 16:26:42 -0500

    add callback collector

--- xivo/pubsub.py
+++ xivo/pubsub.py
@@ -3,8 +3,11 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import logging
+import threading
+import uuid
 
 from collections import defaultdict
+from functools import partial
 
 logger = logging.getLogger(__name__)
 
@@ -50,3 +53,37 @@ class Pubsub(object):
 
         if not self._subscribers[topic]:
             self._subscribers.pop(topic, None)
+
+
+class CallbackCollector(object):
+
+    _TOPIC = 'callback-collector'
+
+    def __init__(self):
+        self._pubsub = Pubsub()
+        self._sources = set()
+        self._lock = threading.Lock()
+
+    def new_source(self):
+        source_id = uuid.uuid4()
+        logger.debug('Creating new callback source "%s"', source_id)
+        self._sources.add(source_id)
+        return partial(self.collect, source_id)
+
+    def collect(self, source_id, *args, **kwargs):
+        logger.debug('Collecting callback source "%s"', source_id)
+        if not self._sources:
+            logger.debug('Aborting collect')
+            return
+
+        with self._lock:
+            self._sources.discard(source_id)
+            if not self._sources:
+                logger.debug('Collecting callbacks finished, publishing')
+                self.publish()
+
+    def publish(self):
+        self._pubsub.publish(self._TOPIC, message=None)
+
+    def subscribe(self, callback):
+        self._pubsub.subscribe(self._TOPIC, lambda arg: callback())
