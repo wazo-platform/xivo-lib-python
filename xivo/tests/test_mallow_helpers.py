96a8c69076d07300cd419863c930773326dbf2dd
Sebastien Duthil <sduthil@wazo.community>
2019-02-06 14:30:04 -0500

    mallow_helpers: add tests for base Schema

--- xivo/tests/test_mallow_helpers.py
+++ xivo/tests/test_mallow_helpers.py
@@ -2,12 +2,20 @@
 # Copyright 2018-2019 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+from collections import OrderedDict
 from unittest import TestCase
 
 from hamcrest import (
     assert_that,
     empty,
     has_entries,
+    calling,
+    raises,
+    instance_of,
+)
+from marshmallow import (
+    fields,
+    ValidationError,
 )
 
 from ..mallow_helpers import ListSchema, Schema
@@ -21,6 +29,25 @@ class TestSchema(TestCase):
         result, error = schema.load(None)
 
         assert_that(result, empty())
+
+    def test_strict(self):
+
+        class TestSchema(Schema):
+
+            key = fields.String(required=True)
+
+        assert_that(calling(TestSchema().load).with_args({}),
+                    raises(ValidationError))
+
+    def test_ordered(self):
+
+        class TestSchema(Schema):
+
+            key2 = fields.String(required=True)
+            key1 = fields.String(required=True)
+
+        result = TestSchema().dump({'key1': '1', 'key2': '2'}).data
+        assert_that(result, instance_of(OrderedDict))
 
 
 class TestListSchema(TestCase):
