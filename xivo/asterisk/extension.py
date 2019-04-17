0d14eef5fb40e1037189693fd52a90608d2937e1
Francois Blackburn <fblackburn@wazo.community>
2019-04-17 17:00:10 -0400

    remove unused function previously used by xivo-ctid

--- xivo/asterisk/extension.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2007-2014 Avencall
-# SPDX-License-Identifier: GPL-3.0-or-later
-
-from collections import namedtuple
-
-
-Extension = namedtuple('FullExtension', ['number', 'context', 'is_internal'])
-Extension.__repr__ = lambda self: '%s@%s' % (self.number, self.context)
