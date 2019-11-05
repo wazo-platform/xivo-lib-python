a5f3db7e4eb8837f3e785e96f746f7238a8a2484
Mehdi Abaakouk <mabaakouk@wazo.io>
2019-11-05 15:26:24 +0100

    config_helpers: Remove useless class
    All methods are classmethod/staticmethod, no need to have classes for
    that.

--- xivo/config_helper.py
+++ xivo/config_helper.py
@@ -28,102 +28,89 @@ class _YAMLExecTag(yaml.YAMLObject):
                     )
 
 
-class ErrorHandler(object):
-    def on_parse_config_file_env_error(self, config_file_name, e):
-        pass
-
-    def on_parse_config_dir_env_error(self, directory_name, e):
-        pass
-
-    def on_parse_config_dir_parse_exception(self, filename, e):
-        pass
+def _on_parse_config_file_env_error(config_file_name, e):
+    print(
+        'Could not read config file {}: {}'.format(config_file_name, e),
+        file=sys.stderr,
+    )
 
 
-class PrintErrorHandler(ErrorHandler):
-    def on_parse_config_file_env_error(self, config_file_name, e):
-        print(
-            'Could not read config file {}: {}'.format(config_file_name, e),
-            file=sys.stderr,
-        )
-
-    def on_parse_config_dir_env_error(self, directory_name, e):
-        print(
-            'Could not read config dir {}: {}'.format(directory_name, e),
-            file=sys.stderr,
-        )
-
-    def on_parse_config_dir_parse_exception(self, filename, e):
-        print('Could not read config file {}: {}'.format(filename, e), file=sys.stderr)
+def _on_parse_config_dir_env_error(directory_name, e):
+    print(
+        'Could not read config dir {}: {}'.format(directory_name, e),
+        file=sys.stderr,
+    )
 
 
-class ConfigParser(object):
-    def __init__(self, error_handler=PrintErrorHandler()):
-        self._error_handler = error_handler
+def _on_parse_config_dir_parse_exception(filename, e):
+    print('Could not read config file {}: {}'.format(filename, e), file=sys.stderr)
 
-    def parse_config_file(self, config_file_name):
-        try:
-            with open(config_file_name) as config_file:
-                data = yaml.load(config_file)
-            return data if data else {}
-        except EnvironmentError as e:
-            self._error_handler.on_parse_config_file_env_error(config_file_name, e)
-            return {}
 
-    def parse_config_dir(self, directory_name):
-        '''
-        Reads all files in directory_name and returns a list of dictionaries containing
-        the parsed yaml content from these files.
+def parse_config_file(config_file_name):
+    try:
+        with open(config_file_name) as config_file:
+            data = yaml.load(config_file)
+        return data if data else {}
+    except EnvironmentError as e:
+        _on_parse_config_file_env_error(config_file_name, e)
+        return {}
 
-        Invalid files are ignored and a message is sent to stderr
-        '''
-        full_path = partial(os.path.join, directory_name)
-        try:
-            extra_config_filenames = os.listdir(directory_name)
-        except EnvironmentError as e:
-            self._error_handler.on_parse_config_dir_env_error(directory_name, e)
-            return []
 
-        def _config_generator():
-            for filename in sorted(extra_config_filenames):
-                if filename.startswith('.'):
-                    continue
-                if not filename.endswith('.yml'):
-                    continue
+def parse_config_dir(directory_name):
+    '''
+    Reads all files in directory_name and returns a list of dictionaries containing
+    the parsed yaml content from these files.
 
-                try:
-                    yield self.parse_config_file(full_path(filename))
-                except Exception as e:
-                    self._error_handler.on_parse_config_dir_parse_exception(filename, e)
+    Invalid files are ignored and a message is sent to stderr
+    '''
+    full_path = partial(os.path.join, directory_name)
+    try:
+        extra_config_filenames = os.listdir(directory_name)
+    except EnvironmentError as e:
+        _on_parse_config_dir_env_error(directory_name, e)
+        return []
 
-        return list(_config_generator())
+    def _config_generator():
+        for filename in sorted(extra_config_filenames):
+            if filename.startswith('.'):
+                continue
+            if not filename.endswith('.yml'):
+                continue
 
-    def read_config_file_hierarchy(
-        self,
-        original_config,
-        config_file_key='config_file',
-        extra_config_dir_key='extra_config_files',
-    ):
-        '''
-        Read a config file and an extra config directory, then return a dictionary
-        containing the config read, aggregated by the following priority:
+            try:
+                yield parse_config_file(full_path(filename))
+            except Exception as e:
+                _on_parse_config_dir_parse_exception(filename, e)
 
-        1. extra config directory (in alphabetical order)
-        2. config file
+    return list(_config_generator())
 
-        The config file name is taken from original_config[config_file_key].
-        The extra config directory name is taken from
-        config_file[extra_config_dir_key] else original_config[extra_config_dir_key].
-        '''
 
-        main_config_filename = original_config[config_file_key]
-        main_config = self.parse_config_file(main_config_filename)
-        extra_config_file_directory = ChainMap(main_config, original_config)[
-            extra_config_dir_key
-        ]
-        configs = self.parse_config_dir(extra_config_file_directory)
-        configs.append(main_config)
+def read_config_file_hierarchy(
+    original_config,
+    config_file_key='config_file',
+    extra_config_dir_key='extra_config_files',
+):
+    '''
+    Read a config file and an extra config directory, then return a dictionary
+    containing the config read, aggregated by the following priority:
 
-        return ChainMap(*configs)
+    1. extra config directory (in alphabetical order)
+    2. config file
+
+    The config file name is taken from original_config[config_file_key].
+    The extra config directory name is taken from
+    config_file[extra_config_dir_key] else original_config[extra_config_dir_key].
+    '''
+
+    main_config_filename = original_config[config_file_key]
+    main_config = parse_config_file(main_config_filename)
+    extra_config_file_directory = ChainMap(main_config, original_config)[
+        extra_config_dir_key
+    ]
+    configs = parse_config_dir(extra_config_file_directory)
+    configs.append(main_config)
+
+    return ChainMap(*configs)
 
 
 class UUIDNotFound(RuntimeError):
@@ -141,10 +128,3 @@ def get_xivo_uuid(logger):
 
 def set_xivo_uuid(config, logger):
     config['uuid'] = get_xivo_uuid(logger)
-
-
-_config_parser = ConfigParser()
-
-parse_config_file = _config_parser.parse_config_file
-parse_config_dir = _config_parser.parse_config_dir
-read_config_file_hierarchy = _config_parser.read_config_file_hierarchy
