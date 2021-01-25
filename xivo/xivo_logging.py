5540bd6a65bcd895adce19b7c37632e28f4cf393
Alexandre Fournier <afournier@wazo.io>
2021-01-25 07:19:42 -0500

    logging: use monkey-patch for fixing logging

--- xivo/xivo_logging.py
+++ xivo/xivo_logging.py
@@ -3,7 +3,12 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import logging
+import os
 import sys
+import traceback
+
+from logging import raiseExceptions
+
 
 DEFAULT_LOG_FORMAT = '%(asctime)s [%(process)d] (%(levelname)s) (%(name)s): %(message)s'
 DEFAULT_LOG_LEVEL = logging.INFO
@@ -12,23 +17,88 @@ DEFAULT_LOG_LEVEL = logging.INFO
 # This is fixed in Python 3.7.4 standard library, but Debian uses 3.7.3
 # See https://github.com/python/cpython/commit/6a7a9f1d83cef628d2bacd71ee568b93f53fd6b4
 
-class StreamHandler(logging.StreamHandler):
 
-    def emit(self, record):
+def StreamHandler_emit(self, record):
+    """
+    Emit a record.
+    If a formatter is specified, it is used to format the record.
+    The record is then written to the stream with a trailing newline.  If
+    exception information is present, it is formatted using
+    traceback.print_exception and appended to the stream.  If the stream
+    has an 'encoding' attribute, it is used to determine how to do the
+    output to the stream.
+    """
+    try:
+        msg = self.format(record)
+        stream = self.stream
+        # issue 35046: merged two stream.writes into one.
+        stream.write(msg + self.terminator)
+        self.flush()
+    except RecursionError:  # See issue 36272
+        raise
+    except Exception:
+        self.handleError(record)
+
+
+def Handler_handleError(self, record):
+    """
+    Handle errors which occur during an emit() call.
+    This method should be called from handlers when an exception is
+    encountered during an emit() call. If raiseExceptions is false,
+    exceptions get silently ignored. This is what is mostly wanted
+    for a logging system - most users will not care about errors in
+    the logging system, they are more interested in application errors.
+    You could, however, replace this with a custom handler if you wish.
+    The record which was being processed is passed in to this method.
+    """
+    if raiseExceptions and sys.stderr:  # see issue 13807
+        t, v, tb = sys.exc_info()
         try:
-            super(StreamHandler, self).emit(record)
-        except RecursionError:
-            raise
+            sys.stderr.write('--- Logging error ---\n')
+            traceback.print_exception(t, v, tb, None, sys.stderr)
+            sys.stderr.write('Call stack:\n')
+            # Walk the stack frame up until we're out of logging,
+            # so as to print the calling context.
+            frame = tb.tb_frame
+            while (
+                frame
+                and os.path.dirname(frame.f_code.co_filename) == logging.__path__[0]
+            ):
+                frame = frame.f_back
+            if frame:
+                traceback.print_stack(frame, file=sys.stderr)
+            else:
+                # couldn't find the right stack frame, for some reason
+                sys.stderr.write(
+                    'Logged from file %s, line %s\n' % (record.filename, record.lineno)
+                )
+            # Issue 18671: output logging message and arguments
+            try:
+                sys.stderr.write(
+                    'Message: %r\nArguments: %s\n' % (record.msg, record.args)
+                )
+            except RecursionError:  # See issue 36272
+                raise
+            except Exception:
+                sys.stderr.write(
+                    'Unable to print the message and arguments'
+                    ' - possible formatting error.\nUse the'
+                    ' traceback above to help find the error.\n'
+                )
+        except OSError:  # pragma: no cover
+            pass  # see issue 5971
+        finally:
+            del t, v, tb
 
-    def handleError(self, record):
-        try:
-            super(StreamHandler, self).handleError(record)
-        except RecursionError:
-            raise
 
-
-class FileHandler(StreamHandler, logging.FileHandler):
-    pass
+def apply_monkeypatch():
+    if (
+        sys.version_info.major == 3
+        and sys.version_info.minor < 8
+        and sys.version_info.micro < 4
+    ):
+        logging.Handler.handleError = Handler_handleError
+        logging.StreamHandler.emit = StreamHandler_emit
 
 
 class _StreamToLogger(object):
@@ -78,20 +148,21 @@ def setup_logging(
     sys.stderr > streamtologger(ERROR) ^        > streamhandler(level>=ERROR) > sys.stderr
                                                 > filehandler(all levels) > file
     """
+    apply_monkeypatch()
     root_logger = logging.getLogger()
 
     formatter = logging.Formatter(log_format)
 
-    handler = FileHandler(log_file)
+    handler = logging.FileHandler(log_file)
     handler.setFormatter(formatter)
     root_logger.addHandler(handler)
 
-    stdout_handler = StreamHandler(sys.stdout)
+    stdout_handler = logging.StreamHandler(sys.stdout)
     stdout_handler.addFilter(_LogLevelFilter(lambda level: level < logging.ERROR))
     stdout_handler.setFormatter(formatter)
     root_logger.addHandler(stdout_handler)
 
-    stderr_handler = StreamHandler(sys.stderr)
+    stderr_handler = logging.StreamHandler(sys.stderr)
     stderr_handler.addFilter(_LogLevelFilter(lambda level: level >= logging.ERROR))
     stderr_handler.setFormatter(formatter)
     root_logger.addHandler(stderr_handler)
@@ -115,7 +186,8 @@ def silence_loggers(logger_names, level):
 def excepthook(exception_class, exception_instance, traceback):
     try:
         logging.getLogger().critical(
-            exception_instance, exc_info=(exception_class, exception_instance, traceback)
+            exception_instance,
+            exc_info=(exception_class, exception_instance, traceback),
         )
     except Exception:
         # If we get an exception here, it means that there was a problem with the logger
@@ -123,7 +195,8 @@ def excepthook(exception_class, exception_instance, traceback):
         sys.stderr = sys.__stderr__
         sys.stdout = sys.__stdout__
         logging.getLogger().critical(
-            exception_instance, exc_info=(exception_class, exception_instance, traceback)
+            exception_instance,
+            exc_info=(exception_class, exception_instance, traceback),
         )
 
 
