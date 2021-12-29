ad4085372dad30a10801ea3a6ce448663876f90d
Francois Blackburn <fblackburn@wazo.community>
2021-12-29 14:36:35 -0500

    rename xivo-test-helpers to wazo-test-helpers

--- integration_tests/suite/test_logging.py
+++ integration_tests/suite/test_logging.py
@@ -1,10 +1,10 @@
-# Copyright 2018-2019 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2018-2021 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import os
 
 from hamcrest import assert_that, contains_string
-from xivo_test_helpers.asset_launching_test_case import AssetLaunchingTestCase
+from wazo_test_helpers.asset_launching_test_case import AssetLaunchingTestCase
 
 ASSET_ROOT = os.path.join(os.path.dirname(__file__), '..', 'assets')
 
