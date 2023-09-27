7cadbfa4f95dca083a3814582e5b78fffba7f27c
Sebastien Duthil <sduthil@wazo.community>
2023-09-27 16:11:26 -0400

    integration tests: mark logs with test start/end

---
+++ integration_tests/suite/conftest.py
@@ -0,0 +1,24 @@
+# Copyright 2023 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import pytest
+
+from pytest import FixtureRequest
+from typing import Iterator
+from wazo_test_helpers.asset_launching_test_case import NoSuchService
+
+
+@pytest.fixture(autouse=True, scope='function')
+def mark_logs(request: FixtureRequest) -> Iterator[None]:
+    test_name = f'{request.cls.__name__}.{request.function.__name__}'
+    try:
+        request.cls.mark_logs_test_start(test_name)
+    except NoSuchService:
+        pass
+
+    yield
+
+    try:
+        request.cls.mark_logs_test_end(test_name)
+    except NoSuchService:
+        pass
