a98563fdf357eb88c44528376ef4740fb9909b4d
Francois Blackburn <fblackburn@wazo.community>
2024-07-15 06:17:37 -0400

    auth_verifier: move flask to its own module
    why: avoid try/except import

---
+++ xivo/flask/auth_verifier.py
@@ -0,0 +1,65 @@
+# Copyright 2024 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0-or-later
+from __future__ import annotations
+
+from functools import wraps
+from typing import Any, Callable, TypeVar
+
+from flask import g
+
+from ..auth_verifier import AuthVerifierHelpers
+from ..http_exceptions import InvalidTokenAPIException, Unauthorized
+from ..tenant_flask_helpers import auth_client, token
+from .headers import extract_token_id_from_header
+
+R = TypeVar('R')
+
+
+class AuthVerifierFlask:
+    def __init__(self) -> None:
+        self.helpers = AuthVerifierHelpers()
+
+    def set_token_extractor(self, func: Callable[..., R]) -> None:
+        endpoint_extract_token = self.helpers.extract_acl_check(func).extract_token_id
+        service_extract_token = extract_token_id_from_header
+        g.token_extractor = endpoint_extract_token or service_extract_token
+
+    def verify_token(self, func: Callable[..., R]) -> Callable[..., R | None]:
+        @wraps(func)
+        def wrapper(*args: Any, **kwargs: Any) -> R | None:
+            if self.helpers.extract_no_auth(func):
+                return func(*args, **kwargs)
+
+            self.set_token_extractor(func)
+            token_uuid = token.uuid
+            required_acl = self.helpers.extract_required_acl(func, kwargs)
+            tenant_uuid = None  # FIXME: Logic not implemented
+
+            self.helpers.validate_token(
+                auth_client,
+                token_uuid,
+                required_acl,
+                tenant_uuid,
+            )
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    def verify_tenant(self, func: Callable[..., R]) -> Callable[..., R]:
+        @wraps(func)
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            required_tenant = self.helpers.extract_required_tenant(func)
+            if not required_tenant:
+                return func(*args, **kwargs)
+
+            self.set_token_extractor(func)
+
+            try:
+                tenant_uuid = token.tenant_uuid
+            except InvalidTokenAPIException:
+                raise Unauthorized(token.uuid)
+
+            self.helpers.validate_tenant(required_tenant, tenant_uuid, token.uuid)
+            return func(*args, **kwargs)
+
+        return wrapper
--- xivo/tests/test_auth_verifier.py
+++ xivo/tests/test_auth_verifier.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import unittest
-from unittest.mock import Mock, PropertyMock, patch
+from unittest.mock import Mock
 from unittest.mock import sentinel as s
 
 import pytest
@@ -16,7 +16,6 @@ from wazo_auth_client.exceptions import (
 from ..auth_verifier import (
     AccessCheck,
     AuthServerUnreachable,
-    AuthVerifier,
     AuthVerifierHelpers,
     Unauthorized,
     no_auth,
@@ -27,104 +26,6 @@ from ..http_exceptions import (
     InvalidTokenAPIException,
     MissingPermissionsTokenAPIException,
 )
-
-
-class TestAuthVerifier(unittest.TestCase):
-    def test_verify_token_decorator(self):
-        mock_client = Mock(host=s.host, port=s.port)
-        auth_verifier = AuthVerifier()
-        mock_token = Mock()
-        mock_g = Mock()
-        g_data = {
-            'auth_client': mock_client,
-            'token': mock_token,
-            'token_extractor': None,
-        }
-        mock_g.get.side_effect = lambda x: g_data[x]
-
-        @auth_verifier.verify_token
-        @required_acl('foo')
-        def decorated():
-            return s.result
-
-        with patch('xivo.auth_verifier.g', mock_g):
-            with patch('xivo.tenant_flask_helpers.g', mock_g):
-                result = decorated()
-
-        assert result == s.result
-
-    def test_verify_token_when_no_auth(self):
-        auth_verifier = AuthVerifier()
-
-        @auth_verifier.verify_token
-        @no_auth
-        def decorated():
-            return s.result
-
-        result = decorated()
-
-        assert result == s.result
-
-    def test_verify_tenant_decorator(self):
-        mock_client = Mock()
-        auth_verifier = AuthVerifier()
-        mock_token = Mock(uuid=s.token, tenant_uuid=s.tenant)
-        mock_g = Mock()
-        g_data = {
-            'auth_client': mock_client,
-            'token': mock_token,
-            'token_extractor': None,
-        }
-        mock_g.get.side_effect = lambda x: g_data[x]
-
-        @auth_verifier.verify_tenant
-        @required_tenant(s.tenant)
-        def decorated():
-            return s.result
-
-        with patch('xivo.auth_verifier.g', mock_g):
-            with patch('xivo.tenant_flask_helpers.g', mock_g):
-                result = decorated()
-
-        assert result == s.result
-
-    def test_verify_tenant_when_no_required_tenant(self):
-        auth_verifier = AuthVerifier()
-
-        @auth_verifier.verify_tenant
-        def decorated():
-            return s.result
-
-        result = decorated()
-
-        assert result == s.result
-
-    def test_verify_tenant_when_invalid_token(self):
-        mock_client = Mock()
-        auth_verifier = AuthVerifier()
-        mock_token = Mock(uuid=s.token)
-        type(mock_token).tenant_uuid = PropertyMock(
-            side_effect=InvalidTokenAPIException(s.token)
-        )
-        mock_g = Mock()
-        g_data = {
-            'auth_client': mock_client,
-            'token': mock_token,
-            'token_extractor': None,
-        }
-        mock_g.get.side_effect = lambda x: g_data[x]
-
-        @auth_verifier.verify_tenant
-        @required_tenant(s.tenant)
-        def decorated():
-            return s.result
-
-        with patch('xivo.auth_verifier.g', mock_g):
-            with patch('xivo.tenant_flask_helpers.g', mock_g):
-                with pytest.raises(Unauthorized) as exc_info:
-                    decorated()
-
-        assert exc_info.value.details['invalid_token'] == s.token
 
 
 class TestAuthVerifierHelpers(unittest.TestCase):
